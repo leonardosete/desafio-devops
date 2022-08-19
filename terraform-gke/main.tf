e9ceedb54430a00a41cfd4a6da5338406259ac9a
Leonardo <leonardo.sete-ext@viavarejo.com.br>
2022-08-19 18:25:55 -0300

    teste 13 - modules

--- terraform-gke/main.tf
+++ terraform-gke/main.tf
@@ -46,7 +46,7 @@ module "gke" {
   subnetwork             = module.gcp-network.subnets_names[0]
   ip_range_pods          = var.ip_range_pods_name
   ip_range_services      = var.ip_range_services_name
-  # release_channel        = var.release_channel 
+  release_channel        = var.release_channel
   node_pools = [
     {
       name                      = "node-pool"
