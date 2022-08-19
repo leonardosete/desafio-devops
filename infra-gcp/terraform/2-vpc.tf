5b5ce7995c1b935a5d363c9ab8f2d82a2c0b2ea4
Leonardo <leonardo.sete-ext@viavarejo.com.br>
2022-08-19 13:09:20 -0300

    teste 4

--- infra-gcp/terraform/2-vpc.tf
+++ infra-gcp/terraform/2-vpc.tf
@@ -1,11 +1,10 @@
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_project_service
 resource "google_project_service" "compute" {
-  service                    = "compute.googleapis.com"
+  service = "compute.googleapis.com"
 }
 
 resource "google_project_service" "container" {
-  service                    = "container.googleapis.com"
-  disable_dependent_services = true
+  service = "container.googleapis.com"
 }
 
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_network
--- infra-gcp/terraform/6-firewalls.tf
+++ infra-gcp/terraform/6-firewalls.tf
@@ -1,6 +1,4 @@
-## ONLY AN EXAMPLE
-## https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_firewall
-
+# # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_firewall
 # resource "google_compute_firewall" "allow-ssh" {
 #   name    = "allow-ssh"
 #   network = google_compute_network.main.name
