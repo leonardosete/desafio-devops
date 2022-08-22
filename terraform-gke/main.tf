27aca6320320e455b288310fe15bfc2652d03d4d
Leonardo <leonardo.sete-ext@viavarejo.com.br>
2022-08-22 17:52:42 -0300

    TEMBICI-SRE - infra - teste 1

--- terraform-gke/main.tf
+++ terraform-gke/main.tf
@@ -59,19 +59,3 @@ module "gke_private-cluster" {
     },
   ]
 }
-
-## CREATE DOCKER REGISTRY
-resource "google_artifact_registry_repository" "repository" {
-  location      = var.region
-  repository_id = "sre-docker-registry"
-  description   = "DOCKER repository"
-  format        = "DOCKER"
-}
-
-## CREATE PYTHON REGISTRY
-resource "google_artifact_registry_repository" "python" {
-  location      = var.region
-  repository_id = "sre-python-registry"
-  description   = "PYTHON repository"
-  format        = "PYTHON"
-}
