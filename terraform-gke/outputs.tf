d7692beaa0d3f13f63cc81d42ae161e41cfd1b6e
Leonardo <leonardo.sete-ext@viavarejo.com.br>
2022-08-21 23:47:50 -0300

    TEMBICI-SRE - infra gke teste 6

--- terraform-gke/outputs.tf
+++ terraform-gke/outputs.tf
@@ -1,9 +1,9 @@
 output "cluster_name" {
   description = "Cluster name"
-  value       = module.tembici-gke_private-cluster.name
+  value       = module.gke_private-cluster.name
 }
 
 output "release_channel" {
   description = "Release Channel"
-  value       = module.tembici-gke_private-cluster.release_channel
+  value       = module.gke_private-cluster.release_channel
 }
