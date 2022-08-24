adfa1340ead4872093ac42807d8fb6dddd47747f
Leonardo <leonardo.sete-ext@viavarejo.com.br>
2022-08-24 11:46:22 -0300

    TEMBICI-SRE - create infra + registry iam member - 5

--- terraform-gke/registry.tf
+++ terraform-gke/registry.tf
@@ -5,10 +5,12 @@ resource "google_artifact_registry_repository" "repository" {
   format        = var.artifact_registry_repository_format
 }
 
-resource "google_artifact_registry_repository_iam_member" "member" {
+resource "google_artifact_registry_repository_iam_binding" "binding" {
   project     = "google_artifact_registry_repository.repository.${var.project_id}"
   location    = "google_artifact_registry_repository.repository.${var.region}"
   repository  = "google_artifact_registry_repository.repository.${var.repository_id}"
   role        = var.artifact_registry_repository_role
-  member      = "serviceAccount:${module.gke_private-cluster.service_account}"
+  members = [
+    "serviceAccount:${module.gke_private-cluster.service_account}",
+  ]
 }
