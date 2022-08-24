6c981a78f13d4bcd28751f956c55dd60b7c087ef
Leonardo <leonardo.sete-ext@viavarejo.com.br>
2022-08-24 12:09:23 -0300

    TEMBICI-SRE - create infra + registry iam member - 6

--- terraform-gke/main.tf
+++ terraform-gke/main.tf
@@ -46,7 +46,8 @@ module "gke_private-cluster" {
   subnetwork             = module.gcp-network.subnets_names[0]
   ip_range_pods          = var.ip_range_pods_name
   ip_range_services      = var.ip_range_services_name
-  release_channel        = "${var.release_channel}"
+  release_channel        = var.release_channel
+  registry_project_ids   = var.repository_id
   
   node_pools = [
     {
--- terraform-gke/registry.tf
+++ terraform-gke/registry.tf
@@ -4,13 +4,3 @@ resource "google_artifact_registry_repository" "repository" {
   repository_id = var.repository_id
   format        = var.artifact_registry_repository_format
 }
-
-resource "google_artifact_registry_repository_iam_binding" "binding" {
-  project     = "google_artifact_registry_repository.repository.${var.project_id}"
-  location    = "google_artifact_registry_repository.repository.${var.region}"
-  repository  = "google_artifact_registry_repository.repository.${var.repository_id}"
-  role        = var.artifact_registry_repository_role
-  members = [
-    "serviceAccount:${module.gke_private-cluster.service_account}",
-  ]
-}
