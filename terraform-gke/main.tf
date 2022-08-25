116a1cc449ed3be1c22d8d922ec41250e5411b53
Leonardo <leonardo.sete-ext@viavarejo.com.br>
2022-08-25 01:43:40 -0300

    TEMBICI-SRE - create infra + registry iam member - 19

--- terraform-gke/main.tf
+++ terraform-gke/main.tf
@@ -10,54 +10,150 @@ resource "local_file" "kubeconfig" {
   filename = "kubeconfig-${var.env_name}"
 }
 
-module "gcp-network" {
-  source       = "terraform-google-modules/network/google"
-  version      = "~> 4.0"
-  project_id   = var.project_id
-  network_name = "${var.network}-${var.env_name}"
-  subnets = [
+## TESTE ##
+module "gke_private-cluster" {
+  source                     = "terraform-google-modules/kubernetes-engine/google//modules/private-cluster"
+  project_id                 = "${var.project_id}"
+  name                       = "${var.cluster_name}-${var.env_name}"
+  region                     = "${var.region}"
+  regional                   = true
+  release_channel            = "${var.release_channel}"
+  network                    = "vpc-01"
+  subnetwork                 = "us-central1-01"
+  ip_range_pods              = "us-central1-01-gke-01-pods"
+  ip_range_services          = "us-central1-01-gke-01-services"
+  http_load_balancing        = false
+  network_policy             = false
+  horizontal_pod_autoscaling = true
+  filestore_csi_driver       = false
+  enable_private_endpoint    = true
+  enable_private_nodes       = true
+  master_ipv4_cidr_block     = "10.0.0.0/28"
+  grant_registry_access      = true
+
+  node_pools = [
     {
-      subnet_name   = "${var.subnetwork}-${var.env_name}"
-      subnet_ip     = "10.10.0.0/16"
-      subnet_region = var.region
+      name                      = "default-node-pool"
+      machine_type              = "e2-medium"
+      node_locations            = "us-central1-b,us-central1-c"
+      min_count                 = 2
+      max_count                 = 6
+      local_ssd_count           = 0
+      spot                      = false
+      disk_size_gb              = 50
+      disk_type                 = "pd-standard"
+      image_type                = "COS_CONTAINERD"
+      enable_gcfs               = false
+      enable_gvnic              = false
+      auto_repair               = true
+      auto_upgrade              = true
+      service_account           = "project-service-account@${var.project_id}.iam.gserviceaccount.com"
+      preemptible               = false
+      initial_node_count        = 3
     },
   ]
-  secondary_ranges = {
-    "${var.subnetwork}-${var.env_name}" = [
+
+  node_pools_oauth_scopes = {
+    all = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+  }
+
+  node_pools_labels = {
+    all = {}
+
+    default-node-pool = {
+      default-node-pool = true
+    }
+  }
+
+  node_pools_metadata = {
+    all = {}
+
+    default-node-pool = {
+      node-pool-metadata-custom-value = "my-node-pool"
+    }
+  }
+
+  node_pools_taints = {
+    all = []
+
+    default-node-pool = [
       {
-        range_name    = var.ip_range_pods_name
-        ip_cidr_range = "10.20.0.0/16"
+        key    = "default-node-pool"
+        value  = true
+        effect = "PREFER_NO_SCHEDULE"
       },
-      {
-        range_name    = var.ip_range_services_name
-        ip_cidr_range = "10.30.0.0/16"
-      },
+    ]
+  }
+
+  node_pools_tags = {
+    all = []
+
+    default-node-pool = [
+      "default-node-pool",
     ]
   }
 }
 
-module "gke_private-cluster" {
-  source                 = "terraform-google-modules/kubernetes-engine/google//modules/private-cluster"
-  project_id             = var.project_id
-  name                   = "${var.cluster_name}-${var.env_name}"
-  regional               = true
-  region                 = var.region
-  network                = module.gcp-network.network_name
-  subnetwork             = module.gcp-network.subnets_names[0]
-  ip_range_pods          = var.ip_range_pods_name
-  ip_range_services      = var.ip_range_services_name
-  release_channel        = var.release_channel
-  # grant_registry_access  = true
-  # # service_account        = var.cluster_admin
-  # registry_project_ids   = [ var.project_id ]
-  node_pools = [
-    {
-      name                      = "node-pool"
-      machine_type              = "e2-medium"
-      node_locations            = "us-central1-b,us-central1-c,us-central1-f"
-      min_count                 = 1
-      max_count                 = 3
-      disk_size_gb              = 50
-    },
-  ]
-}
+## /TESTE/ ##
+
+
+
+# module "gcp-network" {
+#   source       = "terraform-google-modules/network/google"
+#   version      = "~> 4.0"
+#   project_id   = var.project_id
+#   network_name = "${var.network}-${var.env_name}"
+#   subnets = [
+#     {
+#       subnet_name   = "${var.subnetwork}-${var.env_name}"
+#       subnet_ip     = "10.10.0.0/16"
+#       subnet_region = var.region
+#     },
+#   ]
+#   secondary_ranges = {
+#     "${var.subnetwork}-${var.env_name}" = [
+#       {
+#         range_name    = var.ip_range_pods_name
+#         ip_cidr_range = "10.20.0.0/16"
+#       },
+#       {
+#         range_name    = var.ip_range_services_name
+#         ip_cidr_range = "10.30.0.0/16"
+#       },
+#     ]
+#   }
+# }
+
+# module "gke_private-cluster" {
+#   source                 = "terraform-google-modules/kubernetes-engine/google//modules/private-cluster"
+#   project_id             = var.project_id
+#   name                   = "${var.cluster_name}-${var.env_name}"
+#   regional               = true
+#   region                 = var.region
+#   network                = module.gcp-network.network_name
+#   subnetwork             = module.gcp-network.subnets_names[0]
+#   ip_range_pods          = var.ip_range_pods_name
+#   ip_range_services      = var.ip_range_services_name
+#   release_channel        = var.release_channel
+  
+#   create_service_account  = true
+#   grant_registry_access  = true
+#   service_account        = var.cluster_admin
+#   registry_project_ids   = [ var.repository_id ]
+
+#   node_pools = [
+#     {
+#       name                      = "node-pool"
+#       machine_type              = "e2-medium"
+#       node_locations            = "us-central1-b,us-central1-c,us-central1-f"
+#       min_count                 = 1
+#       max_count                 = 3
+#       disk_size_gb              = 50
+#     },
+#   ]
+# }
+
+
