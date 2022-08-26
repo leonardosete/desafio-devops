15aa5cc7906840b4f1bbc24f4f368153ada4de25
Leonardo <leonardo.sete-ext@viavarejo.com.br>
2022-08-26 12:31:10 -0300

    TEMBICI-SRE - gerar infra - teste - 17

--- terraform-gke/data.tf
+++ terraform-gke/data.tf
@@ -1,4 +1,4 @@
 data "external" "gke_service_account" {
-  program = ["bash", "./tembici-desafio-devops/scripts/get-svc-account-name.sh"]
+  program = ["bash", "${path.root}/../scripts/get-svc-account-name.sh"]
   # working_dir = "../"
 }
