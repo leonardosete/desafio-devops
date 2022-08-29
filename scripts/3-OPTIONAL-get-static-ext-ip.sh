3bb65ca1d9129ab27667f829a7a367ed3e712559
Leonardo <leonardo.sete-ext@viavarejo.com.br>
2022-08-29 00:20:22 -0300

    TEMBICI-SRE - novos scripts 2

--- scripts/3-OPTIONAL-get-static-ext-ip.sh
+++ scripts/3-OPTIONAL-get-static-ext-ip.sh
@@ -70,26 +70,53 @@ echo " "
 ### DEFINE VARIABLES DNS ###
 ###############################
 
-    RR_DATA=`gcloud compute addresses list |tail -n1 |awk '{print $2}'` ## I'm using my External Static IP on this case
-    MANAGED_ZONE=`gcloud dns managed-zones list |tail -n1 |awk '{print $1}'`
-    DOMAIN_NAME=`gcloud dns managed-zones list |tail -n1 |awk '{print $2}'`
+    STATIC_EXT_IP=`gcloud compute addresses list |tail -n1 |awk '{print $2}'` ## I'm using my External Static IP on this case
     TTL="300"
     RECORD_TYPE="A"
+    MANAGED_ZONE=`gcloud dns managed-zones list |tail -n1 |awk '{print $1}'`
+    DOMAIN_NAME=`gcloud dns managed-zones list |tail -n1 |awk '{print $2}'`
+    
 
 ###############################
 ### Create a new DNS Record ###
 ###############################
     
-    echo "### The ${RED}DNS Record to be created${NC} example: ${RED}teste${NC}.mydomain.com. ###"
+    echo "### The External Static IP created was ${RED}$STATIC_EXT_IP${NC} ###"
+    echo " " 
+    echo "### The example of RR_DATA is: ${RED}${STATIC_EXT_IP}${NC} ###"
+    echo " " 
+    echo "### The example of DNS_NAME is: ${RED}myapp${NC} ###"
+    echo " "
+    echo "### The default TTL is: ${RED}${TTL}${NC} = equal to 5 minutes ###" 
+    echo " "
+    echo "### The default RECORD_TYPE is: ${RED}${RECORD_TYPE}${NC} - Another types: ${RED}AAAA, ALIAS, MX, CNAME, NS, TXT${NC} and etc... ###"
+    echo " "
+    echo "### The default MANAGED_ZONE is: ${RED}$MANAGED_ZONE${NC} ###" 
+
+
+    read -p "The RR_DATA is: " RR_DATA
+    echo " "
+
     read -p "The DNS_NAME is: " DNS_NAME
     echo " "
 
+    read -p "The TTL is: " TTL
     echo " "
+
+    read -p "The RECORD_TYPE is: " RECORD_TYPE
+    echo " "
+
+    read -p "The MANAGED_ZONE is: " MANAGED_ZONE
+    echo " "
+
+    echo " "
+    echo "### 4-To start a transaction - DNS Record Registration ###"
     gcloud dns record-sets transaction start \
     --zone="$MANAGED_ZONE"
     echo " "
 
     echo " "
+    echo "### 5-To add a record set as part of a transaction ###"
     gcloud dns record-sets transaction add "$RR_DATA" \
     --name="$DNS_NAME.$DOMAIN_NAME" \
     --ttl="$TTL" \
@@ -97,10 +124,13 @@ echo " "
     --zone="$MANAGED_ZONE"
     echo " "
 
-
+    echo " "
+    echo "### 7-To execute the transaction - apply ###"
     gcloud dns record-sets transaction execute \
     --zone="$MANAGED_ZONE"
 
+    echo " "
+    echo "### 8-To execute the transaction ###"
     gcloud dns record-sets list --zone="$MANAGED_ZONE"
 
 ###################
