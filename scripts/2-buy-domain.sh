d860a139963c5df822717479f9522d0129b5a3d4
Leonardo <leonardo.sete-ext@viavarejo.com.br>
2022-08-30 05:13:34 -0300

    TEMBICI-SRE - novos scripts 5

---
+++ scripts/2-buy-domain.sh
@@ -0,0 +1,169 @@
+#!/bin/sh
+
+################
+## ENV COLOR####
+################
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YEL='\033[0;33m'
+NC='\033[0m' # No Color
+
+################
+### WARNING ####
+################
+
+    echo " "
+    echo "### ${YEL}ONLY RUN THIS SCRIPT ${GREEN}IF YOU NEED TO ENABLE${NC} ${YEL}CLOUD DNS PROVIDER SERVICE${NC} ###"
+    echo "### ${YEL}AND BUY A NEW DOMAIN -${NC} ${RED}THIS WILL CAUSE BILLING CHARGES${NC} ###"
+    echo " "
+
+########################
+## INTERACTIVE SCRIPT ##
+########################
+
+    echo " "
+    echo "### ${RED}Check before creating a Cloud DNS Provider and Buying a Domain Name${NC} ###"  
+    read -p "Are you sure about the Cloud DNS Provider and Buying a Domain Name (y/N)? " answer
+    case ${answer:0:1} in
+        y|Y )
+            echo Yes
+        ;;
+        * )
+            echo No
+            exit
+        ;;
+    esac
+
+########################
+## INTERACTIVE SCRIPT ##
+########################
+
+    echo "### The ${RED}New Domain Name:${NC} ###"
+    read -p "The DOMAIN_NAME is: " DOMAIN_NAME
+    echo " "
+
+#########################
+## SEARCH TERMS/DOMAIN ##
+########################
+
+    echo " "
+    echo "### ${YEL}1-Search if the Domain already exists${NC} ###"
+    echo " "
+    gcloud domains registrations search-domains $DOMAIN_NAME
+
+    echo " "
+    echo "### ${YEL}2-To check up-to-date availability for a domain name${NC} ###"
+    echo " "
+    AVAILABILITY=`gcloud domains registrations get-register-parameters $DOMAIN_NAME |grep AVAILABLE`
+    PRICE=`gcloud domains registrations search-domains $DOMAIN_NAME |head -n2 |awk '{print $3}'|tail -n1`
+
+    if [[ $AVAILABILITY == "availability: AVAILABLE" ]]
+    then
+        echo "### The ${GREEN}$DOMAIN_NAME${NC} is ${GREEN}AVAILABLE${NC} for ${YEL}$PRICE USD${NC} ###"
+    else
+        echo "### The ${RED}$DOMAIN_NAME${NC} is ${RED}UNAVAILABLE${NC} ###"
+        echo " "
+        echo "### Choose another ${RED}DOMAIN NAME${NC} SEARCH and run this scripts again ###"
+        exit
+    fi
+
+###########################################################################
+### Check before creating a Cloud DNS Provider and Buying a Domain Name ###
+###########################################################################
+########################
+## /INTERACTIVE SCRIPT ##
+########################
+
+    echo " "
+    echo "### ${RED}Check before creating a Cloud DNS Provider and Buying a Domain Name${NC} ###"  
+
+    read -p "Do you REALLY want to proceed with it? (y/N)? " answer
+    case ${answer:0:1} in
+        y|Y )
+            echo Yes
+        ;;
+        * )
+            echo No
+            exit
+        ;;
+    esac
+
+#########################
+## INTERACTIVE SCRIPT/ ##
+#########################
+    
+    echo "### ${YEL}The Last Domain Name Verified and AVAILABLE is:${NC} ${GREEN}${DOMAIN_NAME}${NC} ###"
+    
+    read -p "Do you want to buy/create it? (y/N)? " answer
+    case ${answer:0:1} in
+        y|Y )
+            echo "### The ${RED}Description's Domain Name:${NC} ###"
+            read -p "The DESCRIPTION is: " DESCRIPTION
+            echo " "
+            
+            echo "### The ${RED}New Cloud DNS Zone Name -${NC} ${YEL}example-com${NC} ###"
+            read -p "The CLOUD_DNS_ZONE_NAME is: " CLOUD_DNS_ZONE_NAME
+            echo " "
+            
+            echo " "
+            echo "### ${YEL}3-Creating a managed public zone for your domain${NC} ###"
+            echo " "
+            gcloud dns managed-zones create $CLOUD_DNS_ZONE_NAME \
+                --description="$DESCRIPTION" \
+                --dns-name=$DOMAIN_NAME
+
+            echo " "
+            echo "### ${YEL}4-To register the domain${NC} ###"
+            echo " "
+            gcloud beta domains registrations register "$DOMAIN_NAME" \
+            --contact-data-from-file=contacts.yaml --contact-privacy=private-contact-data \
+            --yearly-price="12.00 USD" --cloud-dns-zone="$CLOUD_DNS_ZONE_NAME" --quiet
+        ;;
+        * )
+            echo No
+            exit
+        ;;
+    esac
+
+#######################################
+### Verify your contact information ###
+#######################################
+
+   echo " "
+   echo "### ${RED}IMPORTANT${NC} ###"
+   echo " "
+   echo "### ${RED}Verify your contact information${NC} ###"
+   echo " "
+   echo "After you register your domain, Cloud Domains sends a verification email"
+   echo "to the address that you provided in your contact information for the domain."
+   echo " "
+   echo "This email includes a subject line that states Action required:"
+   echo "Please verify your email address."
+   echo " "
+   echo "You must verify your contact information within 15 days or your domain becomes inactive."
+   echo "To verify your email address, follow these steps:"
+   echo " "
+   echo "1. Open the verification email from domains-noreply@google.com."
+   echo "2. Click Verify email now."
+   echo " "
+   echo "Once Cloud Domains verifies your contact information, a Google Domains page"
+   echo "with the message that your email address has been verified is displayed."
+
+
+###################
+## END OF SCRIPT ##
+###################
+
+    echo " "
+    echo " "
+    echo "### ${YEL}Do you want to set a External Static IP and create a DNS Record in Cloud DNS Provider for this project:${NC} ${GREEN}$PROJECT_ID?${NC} ###"
+    read -p "Type y or Y for it - (y/N)? " answer
+    case ${answer:0:1} in
+        y|Y )
+            sh 3-static-ip-and-dns-record.sh
+        ;;
+        * )
+            echo No
+            exit
+        ;;
+    esac
---
+++ scripts/3-static-ip-and-dns-record.sh
@@ -0,0 +1,114 @@
+#!/bin/sh
+
+## The main goal of this script is: ##
+## Sets a External Static IP in GCP and sets in Cloud DNS Provider a DNS Record ##
+## with the new IP created ##
+
+## /vars ##
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+## vars/ ##
+
+
+create_ext_static_ip(){
+## /vars ##
+REGION="us-central1"
+## vars/ ##
+
+echo "### The ${RED}Static External IP${NC} Address Name to be created ###"
+read -p "The ADDRESS_NAME is: " ADDRESS_NAME
+echo " "
+echo " "
+echo "### ${RED}1-Creating a new static external IP${NC} ###"
+echo "### ${RED}Reserving the $ADDRESS_NAME${NC} ###"
+echo " "
+gcloud compute addresses create $ADDRESS_NAME --region="$REGION"
+echo " "
+echo "### ${RED}2-Describe your new static external IP${NC} ###"
+echo " "
+gcloud compute addresses describe $ADDRESS_NAME --region="$REGION"
+}
+
+create_dns_record_entry(){
+## /vars ##
+STATIC_EXT_IP=`gcloud compute addresses list |tail -n1 |awk '{print $2}'` ## I'm using my External Static IP on this case
+TTL="300"
+RECORD_TYPE="A"
+MANAGED_ZONE=`gcloud dns managed-zones list |tail -n1 |awk '{print $1}'`
+DOMAIN_NAME=`gcloud dns managed-zones list |tail -n1 |awk '{print $2}'`
+## vars/ ##
+echo "### The External Static IP created was ${GREEN}$STATIC_EXT_IP${NC} ###"
+echo " " 
+echo "### The example of RR_DATA is: ${RED}${STATIC_EXT_IP}${NC} ###"
+echo " " 
+echo "### The example of DNS_NAME is: ${RED}myapp${NC} ###"
+echo " "
+echo "### The default TTL is: ${RED}${TTL}${NC} = equal to 5 minutes ###" 
+echo " "
+echo "### The default RECORD_TYPE is: ${RED}${RECORD_TYPE}${NC} - Another types: ${RED}AAAA, ALIAS, MX, CNAME, NS, TXT${NC} and etc... ###"
+echo " "
+echo "### The default MANAGED_ZONE is: ${RED}$MANAGED_ZONE${NC} ###" 
+
+read -p "The RR_DATA is: " RR_DATA
+echo " "
+read -p "The DNS_NAME is: " DNS_NAME
+echo " "
+read -p "The TTL is: " TTL
+echo " "
+read -p "The RECORD_TYPE is: " RECORD_TYPE
+echo " "
+read -p "The MANAGED_ZONE is: " MANAGED_ZONE
+echo " "
+echo " "
+
+echo "### 1-To start a transaction - DNS Record Registration ###"
+gcloud dns record-sets transaction start \
+--zone="$MANAGED_ZONE"
+echo " "
+echo " "
+echo "### 2-To add a record set as part of a transaction ###"
+    gcloud dns record-sets transaction add "$RR_DATA" \
+    --name="$DNS_NAME.$DOMAIN_NAME" \
+    --ttl="$TTL" \
+    --type="$RECORD_TYPE" \
+    --zone="$MANAGED_ZONE"
+echo " "
+
+echo " "
+echo "### 3-To execute the transaction - apply ###"
+    gcloud dns record-sets transaction execute \
+    --zone="$MANAGED_ZONE"
+echo " "
+echo "### 4-To execute the transaction ###"
+    gcloud dns record-sets list --zone="$MANAGED_ZONE"
+}
+
+
+### SCRIPT 1 ###
+echo " "
+echo "### ${RED}Check before creating a new Static External IP${NC} ###"
+read -p "### Are you sure about the External Static IP creation? (y/N)" answer
+case ${answer:0:1} in
+    y|Y )
+        create_ext_static_ip
+    ;;
+    * )
+        echo No
+        exit
+    ;;
+esac
+
+### SCRIPT 2 ###
+echo " "
+echo "### ${RED}3-Create a new DNS Record with this IP?${NC} ###"   
+read -p "### Are you sure about this creation? (y/N)" answer
+case ${answer:0:1} in
+    y|Y )
+        create_dns_record_entry
+    ;;
+    * )
+        echo No
+        exit
+    ;;
+esac
