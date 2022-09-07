608f963c9ddc3139213ff4cb2a7217e6a9057060
Leonardo <leonardo.sete-ext@viavarejo.com.br>
2022-09-07 16:46:46 -0300

    TEMBICI-SRE - workflow - teste-fork-17

--- scripts/semantic-version.sh
+++ scripts/semantic-version.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-BRANCH="${GITHUB_REF#refs/heads/}"
+BRANCH="${GITHUB_REF#refs/heads/}" >> $GITHUB_ENV
 # BRANCH="release-teste"
-BRANCH_PREFIX="${BRANCH%%[/-]*}"
+BRANCH_PREFIX="${BRANCH%%[/-]*}" >> $GITHUB_ENV
          
 if [[ $BRANCH_PREFIX == 'release' ]]
-then 
-    SEMANTIC="major" 
+then
+    SEMANTIC="major" >> $GITHUB_ENV
 elif [[ $BRANCH_PREFIX == 'feature' ]]
 then 
-    SEMANTIC="minor"
+    SEMANTIC="minor" >> $GITHUB_ENV
 elif [[ $BRANCH_PREFIX == 'hotfix' ]]
 then 
-    SEMANTIC="patch"
+    SEMANTIC="patch" >> $GITHUB_ENV
 else
   echo "Any branch compatible with semantic version"
   exit 1
 fi
 
-echo ::set-output name=semantic-version::$SEMANTIC
+echo $SEMANTIC
